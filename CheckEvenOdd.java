10f37b07e8fb9a033343942d9ccdf87f4986a25f
ayrshasakeena21 <85064333+ayrshasakeena21@users.noreply.github.com>
2021-06-07 17:42:59 +0530

    Add files via upload

---
+++ CheckEvenOdd.java
@@ -0,0 +1,27 @@
+package lab1;
+
+public class CheckEvenOdd {
+
+	public static void main(String[] args)
+	{
+	  	  int num=20;
+	  	  System.out.println("Enter an integer number:");
+	  	  // The input provided by user is store
+	  	  /* if else is divided by 2 then it's an even number
+	   * else odd number
+	   	   */
+	  if ( num % 2 == 0);
+	  System.out.println("Entered number is even");
+	  System.out.println("Entered number is odd");
+	  
+		  
+		  
+		  
+
+	   
+	   
+	
+
+	}
+
+}
